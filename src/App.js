e5f3982bf9e00e869519eade41010aaeb90edc41
Oswaldo Henriqz <oehero@hotmail.com>
2021-10-12 23:11:00 -0600

    create the app loader

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/app";
+import reducers from "./reducers";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+function main() {
+  ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    document.querySelector(".app-wrapper")
   );
 }
 
-export default App;
+document.addEventListener("DOMContentLoaded", main);
